6fdfcff5d3f7136da9053251da74690f6ff5b356
ContainedChaos <95173963+ContainedChaos@users.noreply.github.com>
2022-10-14 01:55:13 +0600

    student signup updated

--- Signup (Student)/connect.php
+++ Signup (Student)/connect.php
@@ -12,49 +12,71 @@
   $phone = $_POST['phone'];
   $email = $_POST['email'];
   $password = $_POST['password'];
+  $confirm = $_POST['confirm'];
+
+  $regularexpression = "/^[a-zA-Z\d]+$/"
 
 
-  if (!empty($name) && !empty($gender) && !empty($grade) && !empty($version) && !empty($mode) && !empty($area) && !empty($email) && !empty($password))
+  if (!empty($name) && !empty($gender) && !empty($grade) && !empty($version) && !empty($mode) && !empty($area) && !empty($email) && !empty($password) && !empty($confirm))
   {
-    $host = "localhost";
-    $dbUsername = "root";
-    $dbPassword = "";
-    $dbname = "intuition";
-
-    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
-
-    if (mysqli_connect_error())
+    if($password == $confirm)
     {
-      die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+      $host = "localhost";
+      $dbUsername = "root";
+      $dbPassword = "";
+      $dbname = "intuition";
+
+      $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+
+      if (mysqli_connect_error())
+      {
+        die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+      }
+      else
+      {
+        $SELECT = "SELECT email From student Where email = ? Limit 1";
+
+        $INSERT= "INSERT Into student (name, gender, age, address, grade, version, institution, mode, area, salaryrange, phone, email, password) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        $stmt = $conn->prepare($SELECT);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $stmt->bind_result($email);
+        $stmt->store_result();
+        $rnum = $stmt->num_rows;
+
+        if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+          exit('Email is not valid!');
+        }
+
+        if (strlen($_POST['password']) > 15 || strlen($_POST['password']) < 8) {
+          exit('Password must be between 8 and 15 characters long!');
+        }
+
+        if(ctype_upper($_POST['password']) || ctype_lower($_POST['password'])) {
+          exit('Password must contain a combination of uppercase letters, lowercase letters and digits!');
+        }
+
+        if($rnum==0)
+        {
+          $stmt->close();
+
+          $stmt = $conn->prepare($INSERT);
+          $stmt->bind_param("ssssisssssiss", $name, $gender, $age, $address, $grade, $version, $institution, $mode, $area, $salaryrange, $phone, $email, $password);
+          $stmt->execute();
+          echo "New record inserted!";
+
+        }
+        else {
+          echo "Email in use :(";
+        }
+        $stmt->close();
+        $conn->close();
+      }
     }
     else
     {
-      $SELECT = "SELECT email From student Where email = ? Limit 1";
-
-      $INSERT= "INSERT Into student (name, gender, age, address, grade, version, institution, mode, area, salaryrange, phone, email, password) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-
-      $stmt = $conn->prepare($SELECT);
-      $stmt->bind_param("s", $email);
-      $stmt->execute();
-      $stmt->bind_result($email);
-      $stmt->store_result();
-      $rnum = $stmt->num_rows;
-
-      if($rnum==0)
-      {
-        $stmt->close();
-
-        $stmt = $conn->prepare($INSERT);
-        $stmt->bind_param("ssssisssssiss", $name, $gender, $age, $address, $grade, $version, $institution, $mode, $area, $salaryrange, $phone, $email, $password);
-        $stmt->execute();
-        echo "New record inserted!";
-
-      }
-      else {
-        echo "Email in use :(";
-      }
-      $stmt->close();
-      $conn->close();
+      echo "Password fields don't match!";
     }
   }
   else
