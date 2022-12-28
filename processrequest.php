355847da239d6b7ef232980f485e6d99c9c79bd8
ContainedChaos <95173963+ContainedChaos@users.noreply.github.com>
2022-12-29 02:35:29 +0600

    updated tutor slots after request accept

--- processrequest.php
+++ processrequest.php
@@ -20,6 +20,18 @@ if(isset($_POST['accept']))
   $update = "UPDATE requests SET status = 'a' WHERE frommail = '$tutormail' AND tomail = '$studmail'";
   $accepted = mysqli_query($conn, $update);
 
+  $retrievemail = "SELECT * FROM tutor natural join tutorpreferences WHERE email = '$tutormail'";
+  $q1 = mysqli_query($conn, $retrievemail);
+  $row = mysqli_fetch_array($q1);
+  $tutorid = $row['id'];
+  $tutorslots = $row['slots'];
+
+  $tutorslots--;
+
+  $updateslots = "UPDATE tutorpreferences SET slots = '$tutorslots' WHERE id = '$tutorid'";
+  $q2 = mysqli_query($conn, $updateslots);
+  
+
   header("Location: studentviewreq.php");
 }
 
--- processrequest2.php
+++ processrequest2.php
@@ -20,6 +20,17 @@ if(isset($_POST['accept']))
   $update = "UPDATE requests SET status = 'a' WHERE frommail = '$studmail' AND tomail = '$tutormail'";
   $accepted = mysqli_query($conn, $update);
 
+  $retrievemail = "SELECT * FROM tutor natural join tutorpreferences WHERE email = '$tutormail'";
+  $q1 = mysqli_query($conn, $retrievemail);
+  $row = mysqli_fetch_array($q1);
+  $tutorid = $row['id'];
+  $tutorslots = $row['slots'];
+
+  $tutorslots--;
+
+  $updateslots = "UPDATE tutorpreferences SET slots = '$tutorslots' WHERE id = '$tutorid'";
+  $q2 = mysqli_query($conn, $updateslots);
+
   header("Location: tutorviewreq.php");
 }
 
